11abdc7552828c38f2a8113492f5296cdf44202b
rbojorgesv <rafael.bojorge@gmail.com>
2022-05-19 21:12:07 -0600

    portafolio terminado

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//navbar
+function toggleMenu (){
+
+  var menuToggle = document.querySelector ('.toggle');
+  var menu = document.querySelector('.menu');
+  menuToggle.classList.toggle('active')
+  menu.classList.toggle('active')
+}
